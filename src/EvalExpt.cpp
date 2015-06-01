4c36999b305bbd33e93fba4f28c6f89868375aa7
Marc Day <MSDay@lbl.gov>
2015-06-01 10:33:39 -0700

    Retrofit EvalExpt to speak to pltfiles

--- src/EvalExpt.cpp
+++ src/EvalExpt.cpp
@@ -5,78 +5,69 @@
 #include <fstream>
 
 #include <ParmParse.H>
+#include <UqPlotfile.H>
 
 int
 main (int   argc,
       char* argv[])
 {
-//  BoxLib::Initialize(argc,argv); // RG Moved inside driver constructor
-  bool use_synthetic_data = false;
-  Driver driver(argc,argv,use_synthetic_data);
+#ifdef BL_USE_MPI
+  MPI_Init (&argc, &argv);
+  Driver driver(argc,argv,1);
+  driver.SetComm(MPI_COMM_WORLD);
+  driver.init(argc,argv);
+#else
+  Driver driver(argc,argv,0);
+#endif
+
+  ParameterManager& parameter_manager = driver.mystruct->parameter_manager;
   ExperimentManager& expt_manager = driver.mystruct->expt_manager;
-  ParameterManager& param_manager = driver.mystruct->parameter_manager;
-  
-  const std::vector<Real>& true_data = expt_manager.TrueData();
+  expt_manager.SetVerbose(false);
+  expt_manager.SetParallelMode(ExperimentManager::PARALLELIZE_OVER_THREAD);
 
-  std::vector<Real> myparams = param_manager.TrueParameters();
-  int nParams = myparams.size();
+  int nprocs = ParallelDescriptor::NProcs();
+  int myproc = ParallelDescriptor::MyProc();
+
+
   ParmParse pp;
+  std::string sampleFile = "samples"; pp.query("sampleFile",sampleFile);
 
-  Array<std::string> pFabs;
-  int num_pfabs = pp.countval("pFabs");
-  if (pp.countval("pvals")==nParams) {
-    pp.getarr("pvals",myparams,0,nParams);
-  }
-  else if (num_pfabs > 0) {
-    pp.getarr("pFabs",pFabs,0,num_pfabs);
-  }
+  bool verbose = false; pp.query("verbose",verbose);
 
-  int num_datasets = (num_pfabs > 0 ? num_pfabs : 1);
-  int num_data = true_data.size();
-  std::vector<Real> data(num_data);
+  UqPlotfile pf;
+  pf.Read(sampleFile);
+  int iter = pf.ITER();
+  int iters = pf.NITERS();
+  int nwalkers = pf.NWALKERS();
+  int ndim = pf.NDIM();
+  BL_ASSERT(ndim == driver.NumParams());
+  std::vector<Real> samples = pf.LoadEnsemble(iter, iters);
 
-  for (int i=0; i<num_datasets; ++i) {
+  std::cout << std::setprecision(8);
+  std::cout << std::scientific;
+  std::vector<Real> mySamples(ndim);
+  for (int k=0; k<nwalkers; ++k) {
+    for (int t=iter; t<iter+iters; ++t) {
 
-    if (num_pfabs > 0) {
-      if (ParallelDescriptor::IOProcessor()) {
-        std::cout << "...Reading parameters from " << pFabs[i] << std::endl;
-      }
-      std::ifstream ifs; ifs.open(pFabs[i].c_str());
-      FArrayBox pfab;    pfab.readFrom(ifs); ifs.close();
-      BL_ASSERT(pfab.nComp()==nParams);
-      pfab.getVal(&(myparams[0]),pfab.smallEnd(),0,nParams);
-    }
+      int sampleID = nwalkers*(t-iter) + k;
 
-    bool ok = expt_manager.GenerateTestMeasurements(myparams,data);
-  
-    if (ParallelDescriptor::IOProcessor()) {
-
-      if (!ok) {
-        //BoxLib::Abort("Measurements bad");
-      }
-      else {
-        for(int ii=0; ii<num_data; ii++){
-          std::cout << ii << " (" << expt_manager.ExperimentNames()[ii] << ") "
-                    << '\t' << true_data[ii] << '\t' << data[ii] << std::endl;
-        }
+      for (int j=0; j<ndim; ++j) {
+	long index = k + nwalkers*(t-iter) + nwalkers*iters*j;
+	mySamples[j] = samples[index];
       }
 
-      if (pp.countval("outfile") > 0) {
-        std::string outfile; pp.get("outfile",outfile);
-        Box box(IntVect(D_DECL(0,0,0)),
-                IntVect(D_DECL(true_data.size()-1,0,0)));
-        FArrayBox outfab(box,1);
-        for(int ii=0; ii<num_data; ii++){
-          IntVect iv(D_DECL(ii,0,0));
-          outfab(iv,0) = true_data[ii];
-        }
-        std::cout << "Writing data to " << outfile << std::endl;
-        std::ofstream ofs;
-        ofs.open(outfile.c_str());
-        outfab.writeOn(ofs);
-        ofs.close();
+      if (verbose) {
+	std::cout << sampleID << " [";
+	for (int j=0; j<ndim; ++j) {
+	  long index = k + nwalkers*(t-iter) + nwalkers*iters*j;
+	  std::cout << "  " << samples[index];
+	  if (j<ndim-1) std::cout << " ";
+	}
+	std::cout << "]" << std::endl;
       }
+      double myF = driver.LogLikelihood(mySamples);
     }
   }
+
 }
 
